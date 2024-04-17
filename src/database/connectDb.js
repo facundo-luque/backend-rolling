fbad56be80208949b760425f97eba58d3e641919
Facundo Luque <facundo.luque.pro@gmail.com>
2024-04-17 14:03:14 -0300

    variable de entorno dbconnect

--- src/database/connectDb.js
+++ src/database/connectDb.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const dbConection = async () => {
  
     try {
-     await   mongoose.connect("mongodb+srv://facundoluquepro:dl17VKnDfwUeybA0@cluster0.nlub3kx.mongodb.net/Rolling_gim")
+     await   mongoose.connect(process.env.CONECTDB ) 
      
      console.log("Base de datos conectada")
     } catch (error) {
